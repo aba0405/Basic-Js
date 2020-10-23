6777b499519ff98695fcdcb8bccdfb186cae2760
aba0405 <adnan.abdal@gmail.com>
2020-10-23 15:11:35 -0400

    JS-Advanced-Concepts

---
+++ JS-Advanced-Concepts/nullVsUndefined.js
@@ -0,0 +1,12 @@
+//let animal;
+//console.log(animal)
+// function add(num1 , num2){
+//  console.log(num1+num2)
+//  return 
+// }
+
+function add(num1 , num2){
+ console.log(num2)
+}
+const result = add(5,3)
+console.log(result) // Without return the result with come undefined
---
+++ JS-Advanced-Concepts/truthyFalsy.js
@@ -0,0 +1,16 @@
+//const name = "";
+let name = null
+console.log(name)
+// undefined
+// null
+//NaN
+// ""-empty string 
+//0 
+//false
+//above all byDefault false
+if(name){
+ console.log("condition is true")
+}
+else{
+ console.log("condition is false")
+}
