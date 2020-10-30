c7b338966f1e290fbf0ce66b0f4e192e143d360c
aba0405 <adnan.abdal@gmail.com>
2020-10-29 21:45:19 -0400

    M

---
+++ JS-Advanced-Concepts/new.js
@@ -0,0 +1,10 @@
+class Person{
+ constructor(firstName, lastName, Salary){
+  this.firstName = firstName
+  this.lastName = lastName
+  this.Salary = Salary
+ }
+}
+
+const employeePerson = new Person('Philips', 'Bilal', 2000);
+console.log(employeePerson);
